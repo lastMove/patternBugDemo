31f9a27b83d882841b8ecd9a5563650f5caabcbe
lastMove <lastMove@me.com>
2015-04-22 15:43:34 +0200

    Initial Commit

---
+++ TestPatternLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPatternLayer
+//
+//  Created by jason akakpo on 22/04/2015.
+//  Copyright (c) 2015 Big5Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
